4b00106c1b73276dd763da7fb4e916716d23a4ee
juan andres valencia <juanandres2018marzo@gmail.com>
2021-10-18 21:57:47 -0500

    finished, mercadoLiebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 5000
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+module.exports = app
